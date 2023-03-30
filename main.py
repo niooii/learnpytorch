2d700d8eaa8b881a8c5e93cd6918d63ba3b7ccea
John Doe <beyondthestars628@gmail.com>
2023-03-29 20:24:39 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.set_default_device(device)
+print(device)
+tensorA = torch.rand(size=(4, 5, 3))
+tensorB = torch.rand(size=(4, 5, 3))
+print(torch.mm(tensorA, tensorB))
